992d4fa866c07d321e25e0f1da8a5d8ea37181bf
Mark Kharitonov <mark.kharitonov@ceridian.com>
2019-12-27 23:32:57 -0500

    Moved prod and stage to a dedicated git repository

---
+++ prod/services/webserver-cluster/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "mark-kharitonov-terraform-up-and-running-state"
+    key    = "prod/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+module "webserver_cluster" {
+  source        = "../../../modules/services/webserver-cluster"
+  cluster_name  = "webservers-prod"
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 10
+}
+
+resource "aws_autoscaling_schedule" "scale_out_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
---
+++ stage/services/webserver-cluster/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "mark-kharitonov-terraform-up-and-running-state"
+    key    = "stage/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+module "webserver_cluster" {
+  source        = "../../../modules/services/webserver-cluster"
+  cluster_name  = "webservers-stage"
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
+}
